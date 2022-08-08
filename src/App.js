3f44c8cdb2d7400ecae84b8316836b742567f897
Pratyush <pratyushranjan368@gmail.com>
2022-08-08 19:13:07 +0530

    basic coming soon...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-   return <></>;
+   return (
+      <div class="div">
+         <div class="text">
+            <h2>Coming Soon...</h2>
+         </div>
+      </div>
+   );
 };
 
 export default App;
